4df41d8f7b69b6c543db005c306da3cc89698ef1
David Matthams <dmatthams@gmail.com>
2021-04-20 20:05:59 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+ssh-agent -s
 ssh-add <(echo "${INPUT_SSH_KEY}")
 
-rsync -avuh --delete --progress -h -e "ssh -i ~/.ssh/gh_actions_key -o StrictHostKeyChecking=no -p ${INPUT_PORT}" --no-perms --no-owner --no-group --no-times --exclude-from "rsync-ignore.txt" --rsync-path="rsync" ${GITHUB_WORKSPACE}/ ${INPUT_USER}@${INPUT_HOST}:${INPUT_REMOTE_PATH}/deploy-cache
+rsync -avuh --delete --progress -h -e "ssh -o StrictHostKeyChecking=no -p ${INPUT_PORT}" --no-perms --no-owner --no-group --no-times --exclude-from "rsync-ignore.txt" --rsync-path="rsync" ${GITHUB_WORKSPACE}/ ${INPUT_USER}@${INPUT_HOST}:${INPUT_REMOTE_PATH}/deploy-cache
 
 ssh ${INPUT_USER}@${INPUT_HOST} -p ${INPUT_PORT}
 
