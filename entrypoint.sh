0efcc55996e395757cab685750736ae3fb24c886
David Matthams <dmatthams@gmail.com>
2021-04-21 09:58:09 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #echo "Starting ssh agent"
 #eval `ssh-agent -s`
-ssh-agent -a ${ENV_SSH_AUTH_SOCK} > /dev/null
+echo ${ENV_SSH_AUTH_SOCK}
+ssh-add
 #ssh-add <(echo "${INPUT_SSH_KEY}")
 
 #eval $(ssh-agent) &>/dev/null
